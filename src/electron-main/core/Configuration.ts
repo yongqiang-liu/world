bf74c8fc772cc75d22304858a827ffdf99877064
starknt <1431880400@qq.com>
2022-10-23 22:08:00 +0800

    chore: lint

--- src/electron-main/core/Configuration.ts
+++ src/electron-main/core/Configuration.ts
@@ -162,49 +162,49 @@ export default class Configuration extends EventEmitter {
     fs.writeFileSync(
       battleConfigurationPath,
       `
-        {
-          battle: [
-            {
-              id: 0, // 唯一Id
-              name: "紫禁城千场", // Battle名字, 会显示在菜单中
-              stepsId: [824, 300], // 到达目标地点的路过的地图 Id(不包括自己的城市)
-              battleIds: [2315], // 战斗的怪物组 Id, 可传入多个, 当战斗次数满后, 会自动切换
-              battleStep: 120 // 战斗 120 次后回城修理, stepsId 长度 > 0 才有效
-            },
-            {
-              id: 1,
-              name: "破敌(120)",
-              stepsId: [],
-              battleIds: [133],
-              battleStep: 120,
-              max: 120  // 最大战斗次数
-            },
-            {
-              id: 2,
-              name: "屠龙(120)",
-              stepsId: [263, 907],
-              battleIds: [1710],
-              battleStep: 120,
-              max: 120
-            },
-            {
-              id: 3,
-              name: "最强妖兽",
-              stepsId: [5, 820, 819, 821, 822, 823],
-              battleIds: [8, 9, 196],
-              battleStep: 120,
-            },
-            {
-              id: 9999,
-              name: "重复战斗(120)",
-              stepsId: [],
-              battleIds: [],
-              battleStep: 120,
-              max: 120,
-              usePrevBattleId: true // 使用上一次战斗Id
-            },
-          ]
-        }
+{
+  battle: [
+    {
+      id: 0, // 唯一Id
+      name: "紫禁城千场", // Battle名字, 会显示在菜单中
+      stepsId: [824, 300], // 到达目标地点的路过的地图 Id(不包括自己的城市)
+      battleIds: [2315], // 战斗的怪物组 Id, 可传入多个, 当战斗次数满后, 会自动切换
+      battleStep: 120 // 战斗 120 次后回城修理, stepsId 长度 > 0 才有效
+    },
+    {
+      id: 1,
+      name: "破敌(120)",
+      stepsId: [],
+      battleIds: [133],
+      battleStep: 120,
+      max: 120  // 最大战斗次数
+    },
+    {
+      id: 2,
+      name: "屠龙(120)",
+      stepsId: [263, 907],
+      battleIds: [1710],
+      battleStep: 120,
+      max: 120
+    },
+    {
+      id: 3,
+      name: "最强妖兽",
+      stepsId: [5, 820, 819, 821, 822, 823],
+      battleIds: [8, 9, 196],
+      battleStep: 120,
+    },
+    {
+      id: 9999,
+      name: "重复战斗(120)",
+      stepsId: [],
+      battleIds: [],
+      battleStep: 120,
+      max: 120,
+      usePrevBattleId: true // 使用上一次战斗Id
+    },
+  ]
+}
       `,
       {
         flag: 'w+',
