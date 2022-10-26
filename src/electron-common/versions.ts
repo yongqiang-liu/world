2a4901474422d87398cb9769935bca5e2e1e7479
starknt <1431880400@qq.com>
2022-10-26 18:48:53 +0800

    chore: lint

--- src/electron-common/versions.ts
+++ src/electron-common/versions.ts
@@ -3,18 +3,18 @@
  */
 
 export interface Version {
-  name: string;
-  url: string;
+  name: string
+  url: string
 }
 
 export const enum VERSION_KEY {
-  XIAOQI = "小七",
-  TIANYU = "天宇",
-  GUANFANG = "官方",
+  XIAOQI = '小七',
+  TIANYU = '天宇',
+  GUANFANG = '官方',
 }
 
 export interface VersionMap {
-  [key: string]: Version;
+  [key: string]: Version
 }
 
 const VersionMap: VersionMap = {};
@@ -24,18 +24,18 @@ const VersionMap: VersionMap = {};
     VersionMap[name] = {
       name,
       url,
-    };
+    }
   }
 
-  Define(VERSION_KEY.XIAOQI, "http://www.x7sy.com/h5game_play/182.html");
+  Define(VERSION_KEY.XIAOQI, 'http://www.x7sy.com/h5game_play/182.html')
   Define(
     VERSION_KEY.TIANYU,
-    "https://m.tianyuyou.cn/index/h5game_jump.html?tianyuyou_agent_id=10114&game_id=66953"
-  );
+    'https://m.tianyuyou.cn/index/h5game_jump.html?tianyuyou_agent_id=10114&game_id=66953',
+  )
   Define(
     VERSION_KEY.GUANFANG,
-    "https://worldh5.gamehz.cn/version/world/publish/channel/res/index.html"
-  );
-})();
+    'https://worldh5.gamehz.cn/version/world/publish/channel/res/index.html',
+  )
+})()
 
-export default VersionMap;
+export default VersionMap
