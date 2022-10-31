3c5c9ec2efca5c5b73a3b36d2427285e742e4baf
starknt <1431880400@qq.com>
2022-10-31 22:46:14 +0800

    chore: rm scripts

--- scripts/prepackage.js
+++
@@ -1,8 +0,0 @@
-// 删除 config.json
-const fs = require("fs");
-const path = require("path");
-
-const configurationPath = path.join(process.cwd(), "assets", "config.json");
-if (fs.existsSync(configurationPath)) {
-  fs.rmSync(configurationPath);
-}
