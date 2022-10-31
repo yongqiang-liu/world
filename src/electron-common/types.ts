59360f86435caa52a9e7b34e3058881e494f8a0f
starknt <1431880400@qq.com>
2022-10-31 21:23:36 +0800

    feat: daily mission

---
+++ src/electron-common/types.ts
@@ -0,0 +1,42 @@
+export interface AutoMoveMissionPathStep {
+  id: number
+  x: number
+  y: number
+}
+
+export interface Condition {
+  id: number
+  current: number
+  isComplete: boolean
+  total: number
+}
+
+export interface KillMonsterCondition extends Condition {
+  monsterName: string
+}
+
+export interface CollectionCondition extends Condition {
+  name: string
+}
+
+export interface Mission {
+  id: number
+  setting: number
+  level: number
+  mapId: number
+  npcId: number
+  acceptBattleID: number
+  submitBattleID: number
+  getUnCompleteKillMonsterCondition(): KillMonsterCondition | null
+  getUnCompleteCollectionCondition(): CollectionCondition | null
+  isComplete(): boolean
+  isCollectItemType: boolean
+  isKillMonsterMission(): boolean
+  isCanAccept(): boolean
+}
+
+export interface NPC {
+  id: number
+  missions: Mission[]
+  doGetMissionData(func1?: any, func2?: any): any
+}
