9ec772eb92a05d24fe6d3ff7178fb8878068df70
starknt <1431880400@qq.com>
2022-11-02 01:21:33 +0800

    feat: commander system

---
+++ src/electron-renderer/commander.ts
@@ -0,0 +1,19 @@
+import EventEmitter from 'events'
+import type { COMMAND_PAYLOAD } from 'common/Command'
+import { COMMAND } from 'common/Command'
+import { ipcRenderer } from 'electron'
+
+class Commander extends EventEmitter {
+  constructor() {
+    super()
+    this.setMaxListeners(Infinity)
+  }
+}
+
+export function setupCommander() {
+  const commander = new Commander()
+
+  ipcRenderer.on(COMMAND.BASE, (e, payload: COMMAND_PAYLOAD) => {
+    commander.emit(payload.cmd, e, payload)
+  })
+}
